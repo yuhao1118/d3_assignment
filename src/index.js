6d958996a601fd3c8e848a37d4f281b143640b8e
yuhao1118 <1416964247@qq.com>
2020-01-15 12:44:16 +0000

    add interactive elements

--- src/index.js
+++ src/index.js
@@ -15,103 +15,119 @@ class USChoropleth {
     this.fileUrl = fileUrl;
     this.indexColName = indexColName;
     this.dataColName = dataColName;
-    // this.scaleFunction = d3.scaleSequential;
-    // this.interpolateColor = d3.interpolateBlues;
-    this.params = {
-      funcType: 'log',
-      color: 'blue',
-      upBoundary: undefined,
-      lowBoundary: undefined,
-      legendStep: 6
-    };
-    this.funcType = {
-      log: d3.scaleSequentialLog,
-      seque: d3.scaleSequential,
-      pow: d3.scaleSequentialPow,
-      sqrt: d3.scaleSequentialSqrt,
-      symLog: d3.scaleSequentialSymlog,
-      quan: d3.scaleSequentialQuantile
-    };
-    this.interpolateColor = {
-      blue: d3.interpolateBlues,
-      orange: d3.interpolateOranges,
-      green: d3.interpolateGreens,
-      grey: d3.interpolateGreys,
-      purple: d3.interpolatePurples,
-      red: d3.interpolateReds
-    };
+    this.scaleFunction = d3.scaleSequential;
+    this.interpolateColor = d3.interpolateBlues;
+    this.legendStep = 6;
+    this.statisLevel = 'states';
+    this.strokeColor = '#212121';
   }
 
-  setScaleFunction(funcType) {
-    this.params.funcType = funcType;
+  setTitle(title) {
+    this.title = title;
   }
 
-  setColor(color) {
-    this.params.color = color;
+  getTitle() {
+    return this.title;
   }
 
-  setLegendStep(stepsArray) {
-    this.params.legendStep = stepsArray;
+  setFileUrl(fileUrl) {
+    this.fileUrl = fileUrl;
   }
 
-  setRange(lowBoundary, upBoundary) {
-    this.params.lowBoundary = lowBoundary;
-    this.params.upBoundary = upBoundary;
+  getFileUrl() {
+    return this.fileUrl;
   }
 
-  getRange() {
-    return {
-      lowBoundary: this.params.lowBoundary,
-      upBoundary: this.params.upBoundary
-    };
+  setindexColName(indexColName) {
+    this.indexColName = indexColName;
   }
 
-  getLegengStep() {
-    return this.params.legendStep;
+  getIndexColName() {
+    return this.indexColName;
   }
 
-  getScaleFunction() {
-    return this.funcType[this.params.funcType];
+  setDataColName(dataColName) {
+    this.dataColName = dataColName;
   }
 
-  getColor() {
-    return this.interpolateColor[this.params.color];
+  getDataColName() {
+    return this.dataColName;
   }
 
-  getColorScale(usCountyMap) {
-    const scaleFunction = this.getScaleFunction();
+  setStatisLevel(level) {
+    this.statisLevel = level;
+  }
+
+  getStatisLevel() {
+    let obj = { key: this.statisLevel, func: undefined };
+    if (this.statisLevel === 'states') {
+      obj['func'] = d => parseInt(d || 0) * 1000;
+    } else if (this.statisLevel === 'counties') {
+      obj['func'] = d => parseInt(d || 0);
+    }
+    return obj;
+  }
+
+  setScaleFunction(scaleFunction) {
+    this.scaleFunction = scaleFunction;
+  }
+
+  getScaleFunction(usData) {
+    const scaleFunction = this.scaleFunction;
     return scaleFunction(
-      [
-        this.getRange().lowBoundary(usCountyMap) || 1,
-        this.getRange().upBoundary(usCountyMap) || d3.max(usCountyMap)
-      ],
+      [d3.min(usData) || 1, d3.max(usData)],
       this.getColor()
     );
   }
 
+  setColor(interpolateColor) {
+    this.interpolateColor = interpolateColor;
+  }
+
+  getColor() {
+    return this.interpolateColor;
+  }
+
+  setStrokeColor(strokeColor) {
+    this.strokeColor = strokeColor;
+  }
+
+  getStrokeColor() {
+    return this.strokeColor;
+  }
+
+  setLegendStep(legendStep) {
+    this.legendStep = legendStep;
+  }
+
+  getLegengStep() {
+    return this.legendStep;
+  }
+
   getCsv() {
-    return d3.csv(this.fileUrl).then(d => {
+    return d3.csv(this.fileUrl).then(csv => {
+      console.log(this.getDataColName());
       return new Map(
-        d.map(row => [
-          parseInt(row[this.indexColName]),
-          parseFloat(row[this.dataColName])
+        csv.map(row => [
+          parseInt(row[this.getIndexColName()]),
+          parseFloat(row[this.getDataColName()])
         ])
       );
     });
   }
 
   getJson() {
-    return d3.json(this.mapUrl).then(d => {
-      return this.getCsv().then(columnMap => {
-        let usCountyMap = d.objects.states.geometries.map(d => {
-          console.log(parseInt(d.id || 0) * 1000);
-
-          return columnMap.get(parseInt(d.id || 0) * 1000);
+    return d3.json(this.mapUrl).then(json => {
+      return this.getCsv().then(column => {
+        let statisLevel = this.getStatisLevel();
+        let usStateID = json.objects[statisLevel.key].geometries.map(level => {
+          return column.get(statisLevel.func(level.id));
         });
         return {
-          us: d,
-          columnMap: columnMap,
-          usCountyMap: usCountyMap
+          us: json,
+          column: column,
+          usStateID: usStateID,
+          statisLevel: statisLevel
         };
       });
     });
@@ -119,11 +135,15 @@ class USChoropleth {
 
   execute() {
     this.getJson().then(data => {
-      const svg = d3.select('svg').attr('viewBox', [0, 0, 975, 620]);
-
-      const color = this.getColorScale(data.usCountyMap);
-
-
+      d3.select('#title').text(this.getTitle());
+      d3.select('#USChoropleth')
+        .selectAll('*')
+        .remove();
+      const svg = d3
+        .select('svg')
+        .attr('width', '70%')
+        .attr('viewBox', [0, 0, 975, 620]);
+      const colorScale = this.getScaleFunction(data.usStateID);
 
       svg
         .append('path')
@@ -131,7 +151,7 @@ class USChoropleth {
           topojson.mesh(data.us, data.us.objects.states, (a, b) => a !== b)
         )
         .attr('fill', 'none')
-        .attr('stroke', 'white')
+        .attr('stroke', this.getStrokeColor())
         .attr('stroke-linejoin', 'round')
         .attr('d', d3.geoPath());
 
@@ -139,24 +159,26 @@ class USChoropleth {
         .append('g')
         .attr('id', 'usMap')
         .selectAll('path')
-        .data(topojson.feature(data.us, data.us.objects.states).features)
+        .data(
+          topojson.feature(data.us, data.us.objects[data.statisLevel.key])
+            .features
+        )
         .join('path')
-        .attr('fill', d => {
-          return color(data.columnMap.get(parseInt(d.id) * 1000));
-        })
-        .attr('stroke', '#212121')
-        .attr('stroke-width', '1')
-        .attr('d', this.path)
-        .on('mouseover', (d, i) => {
+        .attr('fill', d =>
+          colorScale(data.column.get(data.statisLevel.func(d.id)))
+        )
+        .attr('stroke', this.getStrokeColor())
+        .attr('d', d3.geoPath())
+        .on('mouseover', () => {
           d3.select(d3.event.target).style('opacity', '.6');
         })
-        .on('mouseout', (d, i) => {
+        .on('mouseout', () => {
           d3.select(d3.event.target).style('opacity', '1');
         })
         .append('title')
         .text(d => {
-          return `${d.properties.name}, ${data.columnMap.get(
-            parseInt(d.id) * 1000
+          return `${d.properties.name}, ${data.column.get(
+            data.statisLevel.func(d.id)
           )}`;
         });
 
@@ -166,11 +188,11 @@ class USChoropleth {
         .attr('transform', 'translate(860,400)')
         .call(
           legendColor()
-            .title(this.title)
+            .title(this.getTitle())
             .titleWidth(80)
             .labelFormat(d3.format('.2s'))
             .cells(this.getLegengStep())
-            .scale(color)
+            .scale(colorScale)
         );
 
       svg.call(
@@ -178,28 +200,96 @@ class USChoropleth {
           .zoom()
           .scaleExtent([0.5, 8])
           .on('zoom', () => {
-            svg.selectAll('#usMap').attr('transform', d3.event.transform);
+            svg.select('#usMap').attr('transform', d3.event.transform);
           })
       );
-
-      d3.select('body')
-      .append('button')
-      .text('reset')
-      .on('click', () => {
-        svg.selectAll('#usMap').attr('transform', '1');
-      });
     });
   }
 }
 
-const app = new USChoropleth(
-  'Births_2016',
+var app = new USChoropleth(
+  'US birth number by state (2016)',
   '/static/PopulationEstimates.csv',
   'FIPS',
   'Births_2016'
 );
-app.setColor('blue');
-app.setScaleFunction('seque');
-app.setRange(d3.min, d3.max);
-app.setLegendStep(5);
+
+app.setStrokeColor('white');
 app.execute();
+
+d3.select('#colorDropDown').on('change', () => {
+  let color = d3.event.target.value;
+  let colorMap = {
+    blue: d3.interpolateBlues,
+    orange: d3.interpolateOranges,
+    green: d3.interpolateGreens,
+    grey: d3.interpolateGreys,
+    purple: d3.interpolatePurples,
+    red: d3.interpolateReds
+  };
+
+  app.setColor(colorMap[color]);
+  app.execute();
+});
+
+d3.select('#funcDropDown').on('change', () => {
+  let func = d3.event.target.value;
+  let funcMap = {
+    seque: d3.scaleSequential,
+    sqrt: d3.scaleSequentialSqrt,
+    pow: d3.scaleSequentialPow,
+    log: d3.scaleSequentialLog
+  };
+
+  app.setScaleFunction(funcMap[func]);
+  app.execute();
+});
+
+d3.select('#statisLevel').on('change', () => {
+  app.setStatisLevel(d3.event.target.value);
+  app.execute();
+});
+
+d3.select('#submit').on('click', () => {
+  let title = d3.select('#titleVal').node().value;
+  app.setTitle(title);
+  app.execute();
+});
+
+var clickCount = 0;
+d3.select('#toggle').on('click', () => {
+  clickCount += 1;
+  app =
+    clickCount % 2 == 0
+      ? new USChoropleth(
+          'US birth number by state (2016)',
+          '/static/PopulationEstimates.csv',
+          'FIPS',
+          'Births_2016'
+        )
+      : new USChoropleth(
+          'Unemployment rate by counties',
+          './static/unemployment-x.csv',
+          'id',
+          'rate'
+        );
+
+  if (app.getFileUrl() == '/static/PopulationEstimates.csv') {
+    app.setStatisLevel('states');
+  } else {
+    app.setStatisLevel('counties');
+  }
+
+  app.setStrokeColor('white');
+  app.execute();
+});
+
+d3.select('#reset').on('click', () => {
+  let zoom = d3.zoom().on('zoom', () => {
+    d3.select('#usMap').attr('transform', d3.event.transform);
+  });
+  d3.select('#USChoropleth')
+    .transition()
+    .duration(500)
+    .call(zoom.transform, d3.zoomIdentity);
+});
